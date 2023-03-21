32f6f79c7733af69f758cb4a087d23173831c79c
oleg <pochta@pochta.com>
2023-03-21 23:36:26 +0300

    Add configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:client_it/app/ui/main_app_builder.dart';
 import 'package:client_it/app/ui/main_app_runner.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   const env = String.fromEnvironment("env", defaultValue: "dev");
