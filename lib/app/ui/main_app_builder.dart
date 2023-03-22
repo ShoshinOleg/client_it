2e016aa1591996bc49eb3137b4eef224362fedaa
oleg <pochta@pochta.com>
2023-03-22 19:36:53 +0300

    Initialize AuthCubit. Add GlobalProviders

--- lib/app/ui/main_app_builder.dart
+++ lib/app/ui/main_app_builder.dart
@@ -1,17 +1,39 @@
+import 'package:client_it/app/di/init_di.dart';
 import 'package:client_it/app/domain/app_builder.dart';
+import 'package:client_it/feature/auth/domain/auth_state/auth_cubit.dart';
+import 'package:client_it/feature/auth/domain/repositories/auth_repository.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainAppBuilder implements AppBuilder {
   @override
   Widget buildApp() {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello"),
+    return const _GlobalProviders(
+      child: MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text("Hello"),
+          ),
         ),
-      ),
+      )
     );
   }
+}
 
+class _GlobalProviders extends StatelessWidget {
+  final Widget child;
+
+  const _GlobalProviders({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+              create: (context) => AuthCubit(locator<AuthRepository>())
+          )
+        ],
+        child: child
+    );
+  }
 }
--- lib/feature/auth/domain/auth_state/auth_cubit.dart
+++ lib/feature/auth/domain/auth_state/auth_cubit.dart
@@ -2,9 +2,14 @@ import 'package:client_it/feature/auth/domain/entities/user_entity/user_entity.d
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../repositories/auth_repository.dart';
+
 part 'auth_state.dart';
 part 'auth_cubit.freezed.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit(): super(const AuthState.notAuthorized());
+
+  final AuthRepository authRepository;
+
+  AuthCubit(this.authRepository): super(const AuthState.notAuthorized());
 }
