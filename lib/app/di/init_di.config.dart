664a60ce1efcef12ba4e8aecbb6408f6f486dd82
oleg <pochta@pochta.com>
2023-03-22 16:17:16 +0300

    Add dio

--- lib/app/di/init_di.config.dart
+++ lib/app/di/init_di.config.dart
@@ -5,10 +5,17 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:client_it/app/data/dio_container.dart' as _i5;
+import 'package:client_it/app/data/main_app_config.dart' as _i4;
+import 'package:client_it/app/domain/app_config.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart'
-    as _i2; // ignore_for_file: unnecessary_lambdas
+import 'package:injectable/injectable.dart' as _i2;
 
+const String _prod = 'prod';
+const String _dev = 'dev';
+const String _test = 'test';
+
+// ignore_for_file: unnecessary_lambdas
 // ignore_for_file: lines_longer_than_80_chars
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -21,6 +28,19 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
+    gh.singleton<_i3.AppConfig>(
+      _i4.ProdAppConfig(),
+      registerFor: {_prod},
+    );
+    gh.singleton<_i3.AppConfig>(
+      _i4.DevAppConfig(),
+      registerFor: {_dev},
+    );
+    gh.singleton<_i3.AppConfig>(
+      _i4.TestAppConfig(),
+      registerFor: {_test},
+    );
+    gh.singleton<_i5.DioContainer>(_i5.DioContainer(gh<_i3.AppConfig>()));
     return this;
   }
 }
